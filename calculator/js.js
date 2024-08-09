7f1ac5a415bf69a235a4ba9bd98587755fb5f879
vishnughorela <143708805+vishnughorela@users.noreply.github.com>
2024-08-09 10:29:33 +0530

    Add files via upload

---
+++ calculator/js.js
@@ -0,0 +1,3 @@
+const Number = (num) => document.getElementById('display').value += num;
+const clearDisplay = () => document.getElementById('display').value = '';
+const calculate = () => document.getElementById('display').value = eval(document.getElementById('display').value);
